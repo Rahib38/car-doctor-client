0edbf4e40313e2b0e002e22051b4bb3b2f8470c1
Nadimul Rahib <nadimulrahib38@gmail.com>
2024-05-02 11:16:21 -0700

    added a card

--- src/Pages/Home/ServicesCard.jsx
+++ src/Pages/Home/ServicesCard.jsx
@@ -1,25 +1,25 @@
-
+import { IoMdArrowRoundForward } from "react-icons/io";
 
 const ServicesCard = ({ services }) => {
+  const { title, img, price } = services;
   return (
     <div>
-      <div className="card w-96 bg-base-100 shadow-xl">
-        <figure>
-          <img
-            src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-            alt="Shoes"
-          />
+      <div className="card w-96 h-[400px] bg-base-100 shadow-xl">
+        <figure className="rounded-2xl">
+          <img src={img} alt="Shoes" className="p-4 " />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">
-            Shoes!
-            <div className="badge badge-secondary">NEW</div>
-          </h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
+          <h2 className="card-title">{title}</h2>
+          <div className="flex justify-between items-center">
+            <p className="text-xl text-[#FF3811]">${price}</p>
+            <p className="text-[#FF3811]">
+              <IoMdArrowRoundForward />
+            </p>
+          </div>
+          {/* <div className="card-actions justify-end">
             <div className="badge badge-outline">Fashion</div>
             <div className="badge badge-outline">Products</div>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
